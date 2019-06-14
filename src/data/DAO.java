e30294f2fa37e8e85f51e56a5d7b4ca8d2630fd2
jimmyverdoora <michele@padam.io>
2019-06-14 16:35:40 +0200

    base

---
+++ src/data/DAO.java
@@ -0,0 +1,38 @@
+package data;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+class DAO {
+
+    static List<TO> returnListOfTransferObjects(String query) {
+        List<TO> listOfTransferObjects = new ArrayList<TO>();
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://root:root@192.168.8.55/progetto?serverTimezone=" + TimeZone.getDefault().getID());
+
+            Statement stmt;
+            stmt = conn.createStatement();
+            ResultSet rs;
+            rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                listOfTransferObjects.add(createTransferObject(rs));
+            }
+            stmt.close();
+            conn.close();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return listOfTransferObjects;
+    }
+
+        static TO createTransferObject(ResultSet rs) throws InstantiationException, IllegalAccessException {
+            return null;
+        }
+
+
+}
